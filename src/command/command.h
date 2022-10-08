a358078c329bf11ade09691ec4f423a1e19a6a3a
ensar <ensar.makas@gmail.com>
2022-10-08 13:40:41 +0300

    feature(command): Deleted forking functionalities.
    It is for another branch topic

--- src/command/command.h
+++ src/command/command.h
@@ -6,19 +6,13 @@
 /*   By: emakas <emakas@student.42istanbul.com.t    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/06 13:18:17 by emakas            #+#    #+#             */
-/*   Updated: 2022/10/06 14:57:34 by emakas           ###   ########.fr       */
+/*   Updated: 2022/10/08 13:40:01 by emakas           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef COMMAND_H
 # define COMMAND_H
 # include "../token.h"
-
-struct s_stdio{
-	int	in_new;
-	int	out_new;
-	int	err_new;
-};
 
 struct s_command {
 	char	*command_path;
@@ -54,15 +48,5 @@ void		destroy_command(t_command *command);
  * @return int return code. -1 if execution failed.
  */
 int			execute_default(t_command *self, char **envp);
-
-
-/**
- * @brief Executes command in new process
- * 
- * @param newio 
- * @param command 
- * @return int return code of executed command
- */
-int			execute_forked(struct s_stdio newio, t_command *command);
 
 #endif
