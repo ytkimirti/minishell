1377d1cc4123329a1c118b95d77ddae1ecf89c4d
ensar <ensar.makas@gmail.com>
2022-10-08 14:56:19 +0300

    feature(fork): filled s_stdio and added comment lines

--- src/forking/forking.h
+++ src/forking/forking.h
@@ -6,13 +6,23 @@
 /*   By: emakas <emakas@student.42istanbul.com.t    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/08 13:43:39 by emakas            #+#    #+#             */
-/*   Updated: 2022/10/08 14:47:12 by emakas           ###   ########.fr       */
+/*   Updated: 2022/10/08 14:55:40 by emakas           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+/*
+
+	Purpose of this functionality is, abstracting forking and running
+	proccess. So, whenever command is started in shell, some utilities
+	like forking new proccess, duplicating standart io and recieving
+	last command execution status is handling automatically.
+	
+ */
 
 #ifndef FORKING_H
 # define FORKING_H
 # include "../command/command.h"
+
 struct s_stdio
 {
 	int	in;
@@ -20,7 +30,7 @@ struct s_stdio
 	int	err;
 };
 
-typedef struct s_stdio t_stdio;
+typedef struct s_stdio	t_stdio;
 
 int	forknrun(t_stdio stdio_new, int (*executable)(t_command *cmd, char *envp));
 
