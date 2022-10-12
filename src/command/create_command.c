b9accb1f81d1c72f787f19f9cf0dcc2b49eb7813
ensar <ensar.makas@gmail.com>
2022-10-12 15:36:50 +0300

    update(t_cmd): Implementing t_command functions
    Tokenler için de bir parser tarzı bir şey oluşturmak bana biraz
    mantıklı geldi açıkçası. Token listesi de kendi başına biraz
    anlamsız kaldı gibi.
    farklı fonksiyonlar içinde token listesinde dolaşarak iş yükü
    oluşturmak yerine özel fonksiyonlarca, token listesinden sadece
    istediğimiz bilgileri getirebiliriz kımırtı hojam.
    Hem daha okunabilir olur hem daha kolay kod yazarız gibi.
    
    UYARI:
    
    KOD İÇERİSİNDEKİ BAZI FONKSİYONLAR HENÜZ TANIMLANMADIĞINDAN DOLAYI
    BU COMMIT'İ ANA KODA DAHİL ETMEK TEHLİKELİ VE YASAKTIR

--- src/command/create_command.c
+++ src/command/create_command.c
@@ -6,11 +6,12 @@
 /*   By: emakas <emakas@student.42istanbul.com.t    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/12 14:49:03 by emakas            #+#    #+#             */
-/*   Updated: 2022/10/12 15:00:11 by emakas           ###   ########.fr       */
+/*   Updated: 2022/10/12 15:30:51 by emakas           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "command.h"
+#include "token_adapter.h"
 
 static t_command	*create_system_command(t_token **tokens);
 static t_command	*create_builtin_command(t_token **tokens);
@@ -18,11 +19,25 @@ static t_command	*create_builtin_command(t_token **tokens);
 t_command	*create_command(t_token **tokens)
 {
 	char	*command;
-	int		index;
 
-	index = 0;
-	while (tokens[index] != 0)
-	{}
+	command = get_fitst_word(tokens); // tokenlerden ilk şeyi döndürecek. Ama ne tam ben de bilmiyorum
+
+	if (is_built_in(command))
+	{
+		/* init built in command */
+		return (create_builtin_command(tokens));
+	}
+	else if (in_path(command))
+	{
+		/* init executable command */
+		return (create_system_command(tokens));
+	}
+	else
+	{
+		/* Command %s not found. */
+		printf("Command %s not found",command);
+	}
+
 }
 
 static t_command	*create_system_command(t_token **tokens)
---
+++ src/command/token_adapter.h
@@ -0,0 +1,20 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   token_adapter.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: emakas <emakas@student.42istanbul.com.t    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/10/12 15:14:25 by emakas            #+#    #+#             */
+/*   Updated: 2022/10/12 15:36:44 by emakas           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef TOKEN_ADAPTER
+# define TOKEN_ADAPTER
+# include "../token.h"
+
+char	*get_first_word(t_token **tokens);
+
+
+#endif
