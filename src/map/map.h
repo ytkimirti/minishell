8aa8ebe3c1cae741bcd748c25ab26f707f84bcf5
ensar <ensar.makas@gmail.com>
2022-10-16 14:39:55 +0300

    fix(map): function signatures fixed.

--- src/map/map.h
+++ src/map/map.h
@@ -11,9 +11,9 @@ typedef struct s_map    t_map;
 
 t_map   *create_map(int size);
 void    destroy_map(t_map *map);
-void    map_insert(char *key, char *value);
-void    map_remove(char *key);
-char    *map_get(char *key);
+void    map_insert(t_map *map, char *key, char *value);
+void	map_remove(t_map *map, char *key);
+char	*map_get(t_map *map, char *key);
 t_map	*copy_map(t_map *map1, t_map *map2);
 
 #endif //MAP_H
