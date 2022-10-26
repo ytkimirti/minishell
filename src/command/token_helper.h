6cc290162889b2bfd4faea83c1e9a0230133e719
Yusuf <yusuftaha9@gmail.com>
2022-10-26 17:01:10 +0300

    Fix conflict

--- src/command/token_helper.h
+++ src/command/token_helper.h
@@ -6,23 +6,18 @@
 /*   By: emakas <emakas@student.42istanbul.com.t    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/12 15:14:25 by emakas            #+#    #+#             */
-<<<<<<< Updated upstream
-/*   Updated: 2022/10/20 13:16:36 by emakas           ###   ########.fr       */
-=======
-/*   Updated: 2022/10/26 13:15:41 by emakas           ###   ########.fr       */
->>>>>>> Stashed changes
+/*   Updated: 2022/10/26 16:58:15 by ykimirti         ###   ########.tr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef TOKEN_ADAPTER
+
 # define TOKEN_ADAPTER
+
 # include "../tokenizer/token.h"
 
 t_token 	*get_first_word(t_token **tokens);
-<<<<<<< Updated upstream
 
-=======
 id_t		read_args(t_token **, char ***argv);
->>>>>>> Stashed changes
 
 #endif
