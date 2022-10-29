1079baf20b10b1fe611de376058aef92c135624f
Yusuf <yusuftaha9@gmail.com>
2022-10-29 11:27:09 +0300

    add execute function

---
+++ src/command/execute.c
@@ -0,0 +1,57 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   execute.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ykimirti <ykimirti@42istanbul.com.tr>      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/10/28 17:58:12 by ykimirti          #+#    #+#             */
+/*   Updated: 2022/10/28 19:07:36 by ykimirti         ###   ########.tr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ast.h"
+#include "built_in.h"
+#include "libft.h"
+#include <stdbool.h>
+
+static void	close_std(t_stdio std)
+{
+	if (std.in != 0)
+		close(std.in);
+	if (std.out != 1)
+		close(std.out);
+	if (std.err != 2)
+		close(std.err);
+}
+
+static void	exec_child(t_command *command, t_stdio std)
+{
+	dup2(std.in, 0);
+	dup2(std.out, 1);
+	dup2(std.err, 2);
+	close_std(std);
+	execve(command->argv[0], command->argv, NULL);
+	exit(127);
+}
+
+int	execute_command(t_command *command, t_stdio std, bool is_async)
+{
+	pid_t	pid;
+	int		status;
+
+	if (command->argv[0] == NULL)
+		return (1);
+	if (ft_strncmp(command->argv[0], "echo", sizeof("echo")) == 0)
+		return (ft_echo(command, std));
+	pid = fork();
+	if (pid == 0)
+		exec_child(command, std);
+	close_std(std);
+	if (!is_async)
+	{
+		waitpid(pid, &status, 0);
+		return (WEXITSTATUS(status));
+	}
+	return (0);
+}
