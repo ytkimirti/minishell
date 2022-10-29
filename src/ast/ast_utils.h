88c1df96864254c9c5cc5cdecc6cc6c24d5c9f8a
Yusuf <yusuftaha9@gmail.com>
2022-10-29 11:18:53 +0300

    Move walkers to it's own file, add other walkers, still WIP

--- src/ast/ast_utils.h
+++ src/ast/ast_utils.h
@@ -1,40 +1,25 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ast.h                                              :+:      :+:    :+:   */
+/*   ast_utils.h                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: ykimirti <ykimirti@42istanbul.com.tr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/10/26 14:57:19 by ykimirti          #+#    #+#             */
-/*   Updated: 2022/10/28 19:13:58 by ykimirti         ###   ########.tr       */
+/*   Created: 2022/10/28 16:55:42 by ykimirti          #+#    #+#             */
+/*   Updated: 2022/10/28 19:13:45 by ykimirti         ###   ########.tr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef AST_H
+#ifndef AST_UTILS_H
 
-# define AST_H
+# define AST_UTILS_H
 
-# include "command.h"
-# include "tstdio.h"
+# include "ast.h"
+# include <stdbool.h>
 
-typedef enum e_node_type
-{
-	PIPE,
-	AND,
-	OR,
-	COMMAND
-}	t_node_type;
-
-typedef struct s_node
-{
-	char				*name;
-	struct s_node		*left;
-	struct s_node		*right;
-	enum e_node_type	type;
-	t_command			*command;
-}	t_node;
-
-t_node	*build_example_tree(void);
-int		walk_tree(t_node *tree, t_stdio std);
+int	walk_tree(t_node *tree, t_stdio std);
+int	walk_or(t_node *tree, t_stdio std);
+int	walk_and(t_node *tree, t_stdio std);
+int	walk_pipe(t_node *tree, t_stdio std);
 
 #endif
--- src/ast/tree_builder.c
+++ src/ast/tree_builder.c
@@ -6,13 +6,18 @@
 /*   By: ykimirti <ykimirti@42istanbul.com.tr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/26 15:05:11 by ykimirti          #+#    #+#             */
-/*   Updated: 2022/10/26 16:18:39 by ykimirti         ###   ########.tr       */
+/*   Updated: 2022/10/28 19:33:32 by ykimirti         ###   ########.tr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ast.h"
 #include <stdlib.h>
 #include <stdio.h>
+#include <unistd.h>
+#include <signal.h>
+#include "ast_utils.h"
+#include "command.h"
+#include "libft.h"
 
 t_node	*create_node(t_node_type type, char *name, t_command *cmd, t_node *left, t_node *right)
 {
@@ -23,89 +28,46 @@ t_node	*create_node(t_node_type type, char *name, t_command *cmd, t_node *left, 
 	node->left = left;
 	node->right = right;
 	node->type = type;
+	node->command = cmd;
 	return (node);
 }
 
-int	ft_echo(t_command *command)
+t_command	*create_dummy_command(char *name, char *arg1)
 {
-	printf("%s says hello!\n", command->argv[0]);
-	return (0);
-}
+	t_command	*cmd;
 
-int	ft_false(t_command *command)
-{
-	printf("%s returned false\n", command->argv[0]);
-	(void)command;
-	return (1);
-}
-
-int	ft_true(t_command *command)
-{
-	printf("%s returned true\n", command->argv[0]);
-	(void)command;
-	return (0);
+	cmd = malloc(sizeof(t_command));
+	cmd->argc = 2;
+	cmd->argv = malloc(sizeof(char *) * 3);
+	cmd->argv[0] = ft_strdup(name);
+	cmd->argv[1] = ft_strdup(arg1);
+	cmd->argv[2] = NULL;
+	cmd->redir_file = NULL;
+	cmd->redir_type = 0;
+	return (cmd);
 }
 
 t_node	*build_example_tree()
 {
-	t_command a = {
-		.argv = (char *[]){"a"},
-		.execute = &ft_echo,
-	};
-	t_command b = {
-		.argv = (char *[]){"b"},
-		.execute = &ft_echo,
-	};
-	t_command c = {
-		.argv = (char *[]){"c"},
-		.execute = &ft_echo,
-	};
+	t_command *a, *b, *c;
+	a = create_dummy_command("echo", "hello world");
+	b = create_dummy_command("echo", "asdf");
+	c = create_dummy_command("echo", "asdfasdf");
 
 	return (
 			create_node(PIPE, "top_pipe", NULL,
-				create_node(COMMAND, "a", &a, NULL, NULL),
+				create_node(COMMAND, "a", a, NULL, NULL),
 				create_node(PIPE, "middle_pipe", NULL,
-					create_node(COMMAND, "b", &b, NULL, NULL),
-					create_node(COMMAND, "c", &c, NULL, NULL)
+					create_node(COMMAND, "b", b, NULL, NULL),
+					create_node(COMMAND, "c", c, NULL, NULL)
 					)
 				));
 }
-
-void	fake_pipe(int buf[2])
-{
-	static int	last_pipe = 10;
-
-	buf[0] = last_pipe;
-	buf[1] = last_pipe + 1;
-	last_pipe += 2;
-}
-
-void	walk_tree(t_node *tree, int in, int out, int err)
-{
-	int	pipe[2];
-
-	if (tree->type == PIPE)
-		fake_pipe(pipe);
-	else
-	{
-		pipe[0] = out;
-		pipe[1] = in;
-	}
-
-	if (tree->type == COMMAND)
-	{
-		printf("%s: in: %d -> out: %d err: %d\n", tree->name, in, out, err);
-		return ;
-	}
-	walk_tree(tree->left, in, pipe[0], err);
-	walk_tree(tree->right, pipe[1], out, err);
-}
-
-int	main(void)
-{
-	t_node	*tree;
-
-	tree = build_example_tree();
-
-	walk_tree(tree, 0, 1, 2);
-}
+//
+// int	main(void)
+// {
+// 	t_node	*tree;
+//
+// 	tree = build_example_tree();
+// 	walk_tree(tree, (t_stdio){0, 1, 2}, false);
+// }
