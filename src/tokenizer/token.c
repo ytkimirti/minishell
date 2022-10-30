ef7d62eb6d81f97cf60743f2b271f56a4d44b5cc
Yusuf <yusuftaha9@gmail.com>
2022-10-30 23:18:24 +0300

    what?

--- src/tokenizer/token.c
+++ src/tokenizer/token.c
@@ -1,13 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-<<<<<<<< HEAD:src/tokenizer/token.c
 /*   token.c                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: ykimirti <ykimirti@42istanbul.com.tr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/10/30 22:32:47 by ykimirti          #+#    #+#             */
-/*   Updated: 2022/10/30 22:33:22 by ykimirti         ###   ########.tr       */
+/*   Created: 2022/10/30 23:09:39 by ykimirti          #+#    #+#             */
+/*   Updated: 2022/10/30 23:09:42 by ykimirti         ###   ########.tr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,27 +16,3 @@ inline bool	is_printable(enum e_token_type type)
 {
 	return (type == WORD);
 }
-========
-/*   ast_utils.h                                        :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: ykimirti <ykimirti@42istanbul.com.tr>      +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/10/28 16:55:42 by ykimirti          #+#    #+#             */
-/*   Updated: 2022/10/28 19:13:45 by ykimirti         ###   ########.tr       */
-/*                                                                            */
-/* ************************************************************************** */
-
-#ifndef AST_UTILS_H
-
-# define AST_UTILS_H
-
-# include "ast.h"
-# include <stdbool.h>
-
-int	walk_tree(t_node *tree, t_stdio std);
-int	walk_or(t_node *tree, t_stdio std);
-int	walk_and(t_node *tree, t_stdio std);
-int	walk_pipe(t_node *tree, t_stdio std);
-
-#endif
->>>>>>>> ast:src/ast/ast_utils.h
