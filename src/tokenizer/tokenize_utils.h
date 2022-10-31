ef66a3eba5045ddfc367e57a44bddec4392a40b1
Yusuf <yusuftaha9@gmail.com>
2022-10-31 14:02:39 +0300

    Move is_metacharacter function outside of tokenize_word

---
+++ src/tokenizer/tokenize_utils.h
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   tokenize_utils.h                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ykimirti <ykimirti@42istanbul.com.tr>      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/10/31 13:44:41 by ykimirti          #+#    #+#             */
+/*   Updated: 2022/10/31 13:44:58 by ykimirti         ###   ########.tr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef TOKENIZE_UTILS_H
+
+# define TOKENIZE_UTILS_H
+
+# include <stdbool.h>
+
+bool	is_metacharacter(char c);
+
+#endif
