706d6344a67e5146a7f21760211a75d63bece1fb
Yusuf <yusuftaha9@gmail.com>
2022-11-02 16:35:20 +0300

    make parser support new tokens

--- src/parser/is_command_token.c
+++ src/parser/is_command_token.c
@@ -1,29 +1,24 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   parser_utils.h                                     :+:      :+:    :+:   */
+/*   is_command_token.c                                 :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: ykimirti <ykimirti@42istanbul.com.tr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/10/25 14:19:17 by ykimirti          #+#    #+#             */
-/*   Updated: 2022/11/02 16:07:24 by ykimirti         ###   ########.tr       */
+/*   Created: 2022/11/02 16:07:04 by ykimirti          #+#    #+#             */
+/*   Updated: 2022/11/02 16:21:00 by ykimirti         ###   ########.tr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef PARSER_UTILS_H
+#include "token.h"
+#include <stdlib.h>
 
-# define PARSER_UTILS_H
-
-# include "command.h"
-
-// Calculates the char length the token will take
-// when it's expanded
-// TODO: This doesn't support variable stuff, it
-// needs the environment stuff first for that
-int		length_token(t_token *token);
-
-int		length_tokens(t_token **tokens);
-
-bool	is_command_token(t_token *token);
-
-#endif
+bool	is_command_token(t_token *token)
+{
+	return (token != NULL
+		&& token->type != PIPE_TOKEN
+		&& token->type != PAREN_OPEN
+		&& token->type != PAREN_CLOSE
+		&& token->type != AND_TOKEN
+		&& token->type != OR_TOKEN);
+}
