80c7f1c12417dc3b3517c5a954eeaf6b275fe612
Yusuf <yusuftaha9@gmail.com>
2022-11-03 13:48:12 +0300

    add node_type_tostr

--- src/utils/utils.h
+++ src/utils/utils.h
@@ -6,7 +6,7 @@
 /*   By: ykimirti <ykimirti@42istanbul.com.tr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/06 11:26:50 by ykimirti          #+#    #+#             */
-/*   Updated: 2022/10/31 09:59:17 by ykimirti         ###   ########.tr       */
+/*   Updated: 2022/11/03 13:05:19 by ykimirti         ###   ########.tr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,7 @@
 
 # include "command.h"
 # include "tokenizer.h"
+# include "ast.h"
 
 // Duplicate the str inside token
 // and null terminate it
@@ -27,5 +28,6 @@ void	print_tokens(t_token **tokens);
 // Returns a non freeable string constant
 char	*token_type_tostr(enum e_token_type type);
 char	*redir_type_tostr(t_redir_type type);
+char	*node_type_tostr(t_node_type type);
 
 #endif
---
+++ src/utils/utils_print_2.c
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   utils_print_2.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ykimirti <ykimirti@42istanbul.com.tr>      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/11/03 13:03:25 by ykimirti          #+#    #+#             */
+/*   Updated: 2022/11/03 13:05:00 by ykimirti         ###   ########.tr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ast.h"
+#include "colors.h"
+#include <stdlib.h>
+
+char	*node_type_tostr(t_node_type type)
+{
+	if (type == AND_NODE)
+		return ("AND");
+	if (type == OR_NODE)
+		return ("OR");
+	if (type == PIPE_NODE)
+		return ("PIPE");
+	if (type == COMMAND_NODE)
+		return ("COMMAND");
+	return ("UNKNOWN");
+}
