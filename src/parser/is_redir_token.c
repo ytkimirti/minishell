9486282966cffff1e6f0ecf1cf68f285dd08f04d
Yusuf <yusuftaha9@gmail.com>
2022-11-08 10:09:06 +0300

    add a is_redir_token

--- src/parser/is_redir_token.c
+++ src/parser/is_redir_token.c
@@ -1,31 +1,22 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   parser_utils.h                                     :+:      :+:    :+:   */
+/*   is_redir_token.c                                   :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: ykimirti <ykimirti@42istanbul.com.tr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/10/25 14:19:17 by ykimirti          #+#    #+#             */
-/*   Updated: 2022/11/07 15:55:48 by ykimirti         ###   ########.tr       */
+/*   Created: 2022/11/02 16:07:04 by ykimirti          #+#    #+#             */
+/*   Updated: 2022/11/07 16:00:53 by ykimirti         ###   ########.tr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef PARSER_UTILS_H
+#include "token.h"
+#include <stdlib.h>
 
-# define PARSER_UTILS_H
-
-# include "command.h"
-
-// Calculates the char length the token will take
-// when it's expanded
-// TODO: This doesn't support variable stuff, it
-// needs the environment stuff first for that
-int		length_token(t_token *token);
-
-int		length_tokens(t_token **tokens);
-
-bool	is_command_token(t_token *token);
-
-bool	is_redir_token(t_token *token);
-
-#endif
+bool	is_redir_token(t_token *token)
+{
+	return (token != NULL
+		&& (token->type == REDIR_OUT
+			|| token->type == REDIR_IN
+			|| token->type == REDIR_APPEND));
+}
