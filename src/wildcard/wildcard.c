5b9f885fd62b86a9bf09e46ec942709ba0b93cfe
ensar <ensar.makas@gmail.com>
2022-11-09 17:49:18 +0300

    kahva molası.. düşünemiom daha

--- src/wildcard/wildcard.c
+++ src/wildcard/wildcard.c
@@ -5,13 +5,28 @@
 static char	**find_matches(char *str, int len);
 static char	**search_nodes(char *source, char *pattern, int len_pattern);
 
-// logs/*-12-1999/*.log
+/*!
+ * @brief Lists all matches of given wildcard
+ *
+ * Example wildcard is: "logs/ *-12-1999/ *.log"
+ *
+ * * First get into logs/ and search occurences for "logs"
+ * * if "logs" is a folder (it is) search next pattern matches
+ *   "*-12-1999" inside the matched folders for "logs"
+ *
+ * @param token
+ * @return char** List of all occurences
+ */
 char	**expand_wildcard(t_token *token)
 {
 	char	**matches;
-	char	*workdir;
-
-	// read current working directory. if token starts with root dir. then override workdir.
-	matches = search_nodes(workdir, token->str, token->len);
+	int		index;
+	// We need to iterate through slashes (/)
+	index = 0;
+	while (index < token->len)
+	{
+		// do stuff
+		index = pattern_next(token->str); // finds next pattern 
+	}
 	return (matches);
 }
