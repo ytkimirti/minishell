063793056c177b3305618e6e8297325dccf1aadd
Yusuf <yusuftaha9@gmail.com>
2022-11-10 17:10:08 +0300

    add length to tokenize space

--- src/tokenizer/tokenize_space.c
+++ src/tokenizer/tokenize_space.c
@@ -6,7 +6,7 @@
 /*   By: ykimirti <ykimirti@42istanbul.com.tr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/27 16:40:54 by ykimirti          #+#    #+#             */
-/*   Updated: 2022/11/08 17:11:12 by ykimirti         ###   ########.tr       */
+/*   Updated: 2022/11/10 16:18:12 by ykimirti         ###   ########.tr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,13 +18,17 @@
 t_token	*tokenize_space(char **str, t_state *state)
 {
 	t_token	*token;
+	int		i;
 
 	(void)state;
 	token = (t_token *)malloc(sizeof(t_token));
 	if (token == NULL)
 		malloc_error();
 	token->type = SPACE;
-	while (**str == ' ' || **str == '\t')
-		(*str)++;
+	i = 0;
+	while ((*str)[i] == ' ' || **str == '\t')
+		i++;
+	(*str) += i;
+	token->len = i;
 	return (token);
 }
