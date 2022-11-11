a9ecfc4b4842d2451f88df6da713266e4b362850
Yusuf <yusuftaha9@gmail.com>
2022-11-11 08:29:25 +0300

    optimize skip spaces function

--- src/parser/skip_spaces.c
+++ src/parser/skip_spaces.c
@@ -6,15 +6,16 @@
 /*   By: ykimirti <ykimirti@42istanbul.com.tr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/08 13:04:48 by ykimirti          #+#    #+#             */
-/*   Updated: 2022/11/08 13:04:48 by ykimirti         ###   ########.tr       */
+/*   Updated: 2022/11/11 08:27:35 by ykimirti         ###   ########.tr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "token.h"
 #include "parser_utils.h"
+#include <stdlib.h>
 
 void	skip_spaces(t_token ***tokens)
 {
-	while (is_command_token(**tokens) && (**tokens)->type == SPACE)
+	while ((**tokens) != NULL && (**tokens)->type == SPACE)
 		(*tokens)++;
 }
