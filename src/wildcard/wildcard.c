d6b13d29064db6658bdeaf21641785672e104f0b
ensar <ensar.makas@gmail.com>
2022-11-13 13:35:57 +0300

    implementing wildcard

--- src/wildcard/wildcard.c
+++ src/wildcard/wildcard.c
@@ -1,12 +1,18 @@
 #include "wildcard.h"
 #include "vector.h"
-
+#include <libft.h>
+#define _POSIX_SOURCE
+#include <dirent.h>
+#include <errno.h>
+#include <sys/types.h>
+#undef _POSIX_SOURCE
+#include <stdio.h>
 
 static char	**find_matches(char *str, int len);
 
 static int	compare_pattern(char *source, char *pattern, int len_pattern);
 
-static char	**search_nodes(char *source, char *pattern, int len_pattern);
+static t_pvec *search_nodes(char *source, char *pattern, int len_pattern);
 
 /*!
  * @brief Lists all matches of given wildcard
@@ -33,7 +39,7 @@ char	**expand_wildcard(t_token *token)
  * @brief Searches pattern and returns matches
  *
  * This function is recursive.
- * 
+ *
  * * First get into logs/ and search occurences for "logs"
  * * Assign occurences into MATCHES
  * * iterate to next pattern "*-12-1999"
@@ -43,16 +49,42 @@ char	**expand_wildcard(t_token *token)
  * @param source
  * @param pattern
  * @param len_pattern
- * @return char**
+ * @return t_pvec * char array vector
  */
-static char **search_nodes(char *source, char *pattern, int len_pattern)
+static t_pvec *search_nodes(char *source, char *pattern, int len_pattern)
 {
-	char	*pattern;
-	char	*next_pattern;
-	t_cvec	*matches;
-
-	matches = cvec_new(2);
-
+	struct dirent	*entry
+	t_pvec			*matches;
+	char			*next_pattern;
+	DIR				*dir;
 	
-
+	matches = pvec_new(1);
+	next_pattern = ft_strnstr(pattern,"/",len_pattern);
+	dir = opendir(source);
+	if (dir == NULL)
+		return (NULL);
+	while ((entry = readdir(dir)) != NULL)
+	{
+		if (check_match(entry->d_name, pattern))
+		{
+			if (next_pattern != NULL)
+				recurse(source,entry->d_name,next_pattern,len);
+		}
+	}
+	return (matches);
 }
+/*
+
+def search(pattern,source):
+	matches = list()
+	while ((dir = readdir(source) != NULL)
+		if (checkk_match(dir->name,pattern_first):
+			if (next_pattern != NULL):
+				matches.addAll(search(next_pattern,source + dir->name));
+			else:
+				matches.add(dir->name)
+	return matches
+			
+
+
+*/
