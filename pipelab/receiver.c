c2788af9857e0ed0c1a6d86cf6670d1cea6e0a69
Yusuf <yusuftaha9@gmail.com>
2022-11-13 21:26:59 +0300

    receiver changes

--- pipelab/receiver.c
+++ pipelab/receiver.c
@@ -9,6 +9,7 @@ int	main(int argc, char *argv[])
 	char	buffer[1024];
 	int	count;
 	int	i;
+	int	readed;
 
 	if (argc < 2)
 		count = 0;
@@ -17,9 +18,19 @@ int	main(int argc, char *argv[])
 	i = 0;
 	while (i < count)
 	{
-		if (read(0, buffer, 1) == -1)
+		readed = read(0, buffer, 1);
+		if (readed == -1)
+		{
 			perror("error!");
+			break ;
+		}
+		if (readed == 0)
+		{
+			printf("EOF!\n");
+			break ;
+		}
 		dprintf(2, "receiver: Read '%c'\n", buffer[0]);
 		i++;
 	}
+	printf("Receiver closed!\n");
 }
