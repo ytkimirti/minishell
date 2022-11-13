0e629bfe511dbb9ac8a714d8b1f56ff3d7a2546e
ensar <ensar.makas@gmail.com>
2022-11-13 15:21:09 +0300

    ehehehehhe

--- src/wildcard/wildcard.c
+++ src/wildcard/wildcard.c
@@ -1,18 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   wildcard.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: emakas <emakas@student.42istanbul.com.t    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/11/13 15:08:45 by emakas            #+#    #+#             */
+/*   Updated: 2022/11/13 15:19:20 by emakas           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "wildcard.h"
 #include "vector.h"
 #include <libft.h>
-#define _POSIX_SOURCE
 #include <dirent.h>
 #include <errno.h>
-#include <sys/types.h>
-#undef _POSIX_SOURCE
 #include <stdio.h>
 
-static char	**find_matches(char *str, int len);
+static char		**find_matches(char *str, int len);
 
-static int	compare_pattern(char *source, char *pattern, int len_pattern);
+static int		compare_pattern(char *source, char *pattern, int len_pattern);
 
-static t_pvec *search_nodes(char *source, char *pattern, int len_pattern);
+static t_pvec	*search_nodes(char *source, char *pattern, int len_pattern);
+
+static int		check_match(char *source, char *pattern);
+
+static t_pvec	*recurse(char *source, char *dir, char *pattern, size_t len_p);
 
 /*!
  * @brief Lists all matches of given wildcard
@@ -25,13 +38,15 @@ static t_pvec *search_nodes(char *source, char *pattern, int len_pattern);
  */
 char	**expand_wildcard(t_token *token)
 {
+	t_pvec	*match_vector;
 	char	**matches;
 	char	*pattern;
-	char	*source; // absolute path prefix. If relative path given, this source prefixes before pattern
-	int		*pattern_len; // length of pattern;
-	
-	matches = search_nodes(source, pattern, pattern_len);
+	char	*source;
+	int		*pattern_len;
 
+	match_vector = search_nodes(source, pattern, pattern_len);
+	matches = (char **) match_vector->arr;
+	free(match_vector);
 	return (matches);
 }
 
@@ -51,25 +66,33 @@ char	**expand_wildcard(t_token *token)
  * @param len_pattern
  * @return t_pvec * char array vector
  */
-static t_pvec *search_nodes(char *source, char *pattern, int len_pattern)
+static t_pvec	*search_nodes(char *source, char *pattern, size_t len_pattern)
 {
-	struct dirent	*entry
+	struct dirent	*entry;
 	t_pvec			*matches;
 	char			*next_pattern;
+	size_t			*len_next_pattern
 	DIR				*dir;
-	
+
 	matches = pvec_new(1);
-	next_pattern = ft_strnstr(pattern,"/",len_pattern);
+	next_pattern = ft_strnstr(pattern, "/", len_pattern) + 1;
+	len_next_pattern = len_pattern - (next_pattern - pattern);
 	dir = opendir(source);
 	if (dir == NULL)
-		return (NULL);
-	while ((entry = readdir(dir)) != NULL)
+		return (matches);
+	entry = readdir(dir);
+	while (entry != NULL)
 	{
 		if (check_match(entry->d_name, pattern))
 		{
-			if (next_pattern != NULL)
-				recurse(source,entry->d_name,next_pattern,len);
+			if (len_next_pattern > 0)
+				append_all(matches,
+					recurse(source,
+					entry->d_name,next_pattern,len_next_pattern));
+			else
+				append_vector(matches,source,entry->d_name);
 		}
+		entry = readdir(dir);
 	}
 	return (matches);
 }
