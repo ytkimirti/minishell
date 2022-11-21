a71b1ef48957f23fb0d2c43944a1c47e4391a278
Yusuf <yusuftaha9@gmail.com>
2022-11-21 14:06:51 +0300

    add testing codes

---
+++ pipelab/sender.c
@@ -0,0 +1,47 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   sender.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ykimirti <ykimirti@42istanbul.com.tr>      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/11/12 16:02:58 by ykimirti          #+#    #+#             */
+/*   Updated: 2022/11/14 23:02:23 by ykimirti         ###   ########.tr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/signal.h>
+#include <unistd.h>
+#include <signal.h>
+
+void	on_sigpipe(int sig)
+{
+	dprintf(2, "Got a sigpipe!\n");
+}
+
+int	main(int argc, char *argv[])
+{
+	int	i;
+
+	if (argc < 2)
+		exit(1);
+	i = 0;
+	signal(SIGPIPE, on_sigpipe);
+	signal(SIGINT, on_sigpipe);
+	while (argv[1][i] != '\0')
+	{
+		char c = argv[1][i];
+		if (c == 'n')
+			c = '\n';
+		if (write(1, &c, 1) == -1)
+		{
+			perror("Write error!");
+			break ;
+		}
+		dprintf(2, "Written: %c\n", argv[1][i]);
+		i++;
+	}
+	dprintf(2, "Sender closed!\n");
+}
