fa0746dae8604b16d4dc3ee08463543afb3185fd
Yusuf <yusuftaha9@gmail.com>
2022-11-21 17:11:40 +0300

    add echo -n

--- src/built_in/ft_echo.c
+++ src/built_in/ft_echo.c
@@ -6,7 +6,7 @@
 /*   By: ykimirti <ykimirti@42istanbul.com.tr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/28 06:59:44 by ykimirti          #+#    #+#             */
-/*   Updated: 2022/11/12 20:07:00 by ykimirti         ###   ########.tr       */
+/*   Updated: 2022/11/21 17:11:06 by ykimirti         ###   ########.tr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,13 +17,21 @@
 int	ft_echo(t_command *command, t_stdio std)
 {
 	char	**str;
+	bool	send_newline;
 
 	str = command->argv + 1;
+	send_newline = true;
+	if (*str != NULL && ft_strncmp(*str, "-n", 3) == 0)
+	{
+		send_newline = false;
+		str++;
+	}
 	while (*str != NULL)
 	{
 		ft_putstr_fd(*str, std.out);
 		str++;
 	}
-	write(std.out, "\n", 1);
+	if (send_newline)
+		write(std.out, "\n", 1);
 	return (0);
 }
