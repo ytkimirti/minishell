e098fcae982a9cd181f164f7d51b66ea4f8c7692
Yusuf <yusuftaha9@gmail.com>
2022-11-21 17:30:16 +0300

    add pwd

---
+++ src/built_in/ft_pwd.c
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_pwd.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ykimirti <ykimirti@42istanbul.com.tr>      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/11/21 17:12:09 by ykimirti          #+#    #+#             */
+/*   Updated: 2022/11/21 17:28:58 by ykimirti         ###   ########.tr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ast.h"
+#include "built_in.h"
+#include "libft.h"
+#include "env.h"
+#include <errno.h>
+#include <stdio.h>
+#include <string.h>
+
+int	ft_pwd(t_command *command, t_stdio std)
+{
+	const char	*str;
+
+	(void)command;
+	str = get_env("PWD", 3);
+	if (str == NULL)
+	{
+		ft_putendl_fd("PWD environment variable is not set!", std.err);
+		return (1);
+	}
+	ft_putendl_fd(str, std.out);
+	return (0);
+}
