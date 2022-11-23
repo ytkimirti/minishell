6c33f16850cc845b35e97cbf7b39fe2497c0bdb0
Yusuf <yusuftaha9@gmail.com>
2022-11-23 09:18:09 +0300

    fix bug in get_env returning NULL

--- src/env/get_env.c
+++ src/env/get_env.c
@@ -6,7 +6,7 @@
 /*   By: ykimirti <ykimirti@42istanbul.com.tr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/01 10:59:59 by ykimirti          #+#    #+#             */
-/*   Updated: 2022/11/01 11:43:37 by ykimirti         ###   ########.tr       */
+/*   Updated: 2022/11/23 08:58:16 by ykimirti         ###   ########.tr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,6 +21,6 @@ const char	*get_env(const char *key, int len)
 	map = get_env_singleton();
 	entry = map_get(map, hashcode((void *)key, len));
 	if (entry == NULL)
-		return (NULL);
+		return ("");
 	return (entry->value);
 }
