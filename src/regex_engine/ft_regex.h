6ed2e7273b8e95b2c7cc52e59aa0d5598fa505d6
ensar <ensar.makas@gmail.com>
2022-11-24 21:35:31 +0300

    regex isim değişikliği

--- src/regex_engine/ft_regex.h
+++ src/regex_engine/ft_regex.h
@@ -1,17 +1,17 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   regex.h                                            :+:      :+:    :+:   */
+/*   ft_regex.h                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: emakas <emakas@student.42istanbul.com.t    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/20 18:04:05 by emakas            #+#    #+#             */
-/*   Updated: 2022/11/20 18:04:33 by emakas           ###   ########.fr       */
+/*   Updated: 2022/11/24 21:34:38 by emakas           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef REGEX_H
-# define REGEX_H
+#ifndef FT_REGEX_H
+# define FT_REGEX_H
 
 int	match_string(char *str, char *regex);
 
