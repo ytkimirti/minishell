71e0f88c9d3b75a2f667f57f96d5518611a25df6
Yusuf <yusuftaha9@gmail.com>
2022-12-04 19:08:42 +0300

    remove comment

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: ykimirti <ykimirti@42istanbul.com.tr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/27 15:18:05 by ykimirti          #+#    #+#             */
-/*   Updated: 2022/11/27 13:54:56 by ykimirti         ###   ########.tr       */
+/*   Updated: 2022/12/04 19:02:20 by ykimirti         ###   ########.tr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -50,7 +50,6 @@ static void	shell_loop(void)
 		begin_trace(tokens, line);
 		execute_tokens(tokens);
 		end_trace();
-		// execute_and_set_status(tree);
 		while (waitpid(-1, 0, 0) != -1)
 			;
 		free_tokens(tokens);
