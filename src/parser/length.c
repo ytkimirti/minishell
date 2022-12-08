d4ca990c8f5221deca08b2947a6c01d44f90ff73
Yusuf <yusuftaha9@gmail.com>
2022-12-08 15:03:55 +0300

    Add support for wildcard into various stuff

--- src/parser/length.c
+++ src/parser/length.c
@@ -6,7 +6,7 @@
 /*   By: ykimirti <ykimirti@42istanbul.com.tr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/30 23:42:08 by ykimirti          #+#    #+#             */
-/*   Updated: 2022/11/26 22:14:37 by ykimirti         ###   ########.tr       */
+/*   Updated: 2022/12/08 15:02:36 by ykimirti         ###   ########.tr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,9 +14,15 @@
 #include "env.h"
 #include <stdlib.h>
 #include "parser_utils.h"
+#include <assert.h>
 
+/*
+ * Length of the wildcard token should not be calculated here since it will
+ * not be stringified like other tokens. It's just a seperator.
+ */
 int	length_token(t_token *token)
 {
+	assert(token->type != WILDCARD_TOKEN);
 	if (token->type == WORD)
 		return (token->len);
 	if (token->type == VAR)
