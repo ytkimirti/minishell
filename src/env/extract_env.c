6d3b9290e297f30726e236b3be860a8f032d4911
Yusuf <yusuftaha9@gmail.com>
2023-01-09 16:16:57 +0300

    Comment out merge_sort

--- src/env/extract_env.c
+++ src/env/extract_env.c
@@ -6,7 +6,7 @@
 /*   By: emakas <emakas@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/01 11:02:17 by ykimirti          #+#    #+#             */
-/*   Updated: 2023/01/08 18:57:50 by emakas           ###   ########.fr       */
+/*   Updated: 2023/01/09 16:12:19 by ykimirti         ###   ########.tr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,25 +18,25 @@ static const char	*select_func(t_envdata *data)
 	return (data->key);
 }
 
-static char	**merge_sort(char **arr, int first, int last)
-{
-	int middle;
-
-	middle = (first + last) / 2;
-	if (first > last)
-		return arr;
-	if (last - first > 1)
-	{
-		merge_sort(arr, first, middle);
-		merge_sort(arr, middle + 1 , last);
-	}
-}
-
-static char	**sort_array(char **arr)
-{
-	return (arr);
-}
-
+// static char	**merge_sort(char **arr, int first, int last)
+// {
+// 	int middle;
+//
+// 	middle = (first + last) / 2;
+// 	if (first > last)
+// 		return arr;
+// 	if (last - first > 1)
+// 	{
+// 		merge_sort(arr, first, middle);
+// 		merge_sort(arr, middle + 1 , last);
+// 	}
+// }
+//
+// static char	**sort_array(char **arr)
+// {
+// 	return (arr);
+// }
+//
 char	**extract_env(void)
 {
 	t_map	*map;
@@ -44,5 +44,5 @@ char	**extract_env(void)
 
 	map = get_env_singleton();
 	envp = (char **)map_select(map, (void *(*)(void *))select_func);
-	return (sort_array(envp));
+	return (envp);
 }
